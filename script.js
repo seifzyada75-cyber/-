6c0c886403033dbd341ffc5a6228c6fbd1f4422a
seifzyada75-cyber <seifzyada75@gmail.com>
2026-08-21 23:20:44 +0300

    update

--- script.js
+++ script.js
@@ -199,7 +199,9 @@ cancelTripBtn.addEventListener('click', () => {
 // Network Status Alert
 window.addEventListener('offline', () => alert('عفواً، تم قطع الاتصال بالإنترنت'));
 // تعبئة الأماكن تلقائياً
-
+const locations = ["محطة الرمل، الإسكندرية", "سيدي بشر، الإسكندرية", "سموحة، الإسكندرية", "ميامي، الإسكندرية"];
+const pickupSelect = document.getElementById('pickup-select');
+const dropoffSelect = document.getElementById('dropoff-select');
 
 locations.forEach(loc => {
     pickupSelect.add(new Option(loc, loc));
